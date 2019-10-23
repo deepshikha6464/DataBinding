443815445570f1fad174ae3cdcdcca95cdb80cde
Deepshikha <deepshikha6464@gmail.com>
2019-10-23 21:19:55 +0530

    image addition

--- app/src/main/java/com/example/databinding/Model/User.java
+++ app/src/main/java/com/example/databinding/Model/User.java
@@ -1,12 +1,31 @@
 package com.example.databinding.model;
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
 import androidx.databinding.BaseObservable;
 import androidx.databinding.Bindable;
+import androidx.databinding.BindingAdapter;
 import androidx.databinding.library.baseAdapters.BR;
 
 public class User extends BaseObservable {
     private static final String TAG = "User";
 
-    private  String name , email;
+    private  String name;
+    private String email;
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    private String profileImage;
+
 
     public User(String name, String email) {
         this.name = name;
@@ -35,6 +54,13 @@ public class User extends BaseObservable {
         notifyPropertyChanged(BR.email);
     }
 
+    @BindingAdapter({"android:profileImage"})
+    public static  void loadImage(ImageView view , String imageUrl)
+    {
+        Glide.with(view.getContext())
+                .load("https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiZz4Hs2bLlAhXJXCsKHZG5CA8QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fbeauty%2F&psig=AOvVaw2WvJPZWqtv4D9IestS8pxB&ust=1571931095635511")
+                .into(view);
+    }
     @Override
     public String toString() {
         return "User{" +
