d7234af0869e516ed18f0577cafe937ceb9bfaa3
Deepshikha <deepshikha6464@gmail.com>
2019-10-23 17:36:35 +0530

    observable user class

--- app/src/main/java/com/example/databinding/Model/User.java
+++ app/src/main/java/com/example/databinding/Model/User.java
@@ -1,7 +1,12 @@
 package com.example.databinding.model;
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import androidx.databinding.library.baseAdapters.BR;
 
-public class User {
-    String name , email;
+public class User extends BaseObservable {
+    private static final String TAG = "User";
+
+    private  String name , email;
 
     public User(String name, String email) {
         this.name = name;
@@ -11,20 +16,23 @@ public class User {
     public User() {
     }
 
+    @Bindable
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        notifyPropertyChanged(BR.name);
     }
-
+@Bindable
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+        notifyPropertyChanged(BR.email);
     }
 
     @Override
--- app/src/main/java/com/example/databinding/handlers/ClickListener.java
+++ app/src/main/java/com/example/databinding/handlers/ClickListener.java
@@ -1,11 +1,17 @@
 package com.example.databinding.handlers;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.databinding.MainActivity;
+import com.example.databinding.R;
+import com.example.databinding.databinding.ActivityMainBinding;
 import com.example.databinding.model.User;
+
+import androidx.databinding.DataBindingUtil;
 
 public class ClickListener {
 
@@ -22,6 +28,10 @@ public class ClickListener {
     public void onFABClick(View view)
     {
         Log.d(TAG, "onFABClick: ");
+        ActivityMainBinding binding = DataBindingUtil.setContentView((Activity) view.getContext(), R.layout.activity_main);
+        User user = new User("observable", "qwert@123.com");
+        binding.setUser(user);
+
         Toast.makeText(view.getContext(),"FAB clicked", Toast.LENGTH_SHORT).show();
     }
 
